d7479ad32d1295f1d609a47bc1093c6248b288bb
Alex <alexkhundongbam260@gmail.com>
2025-07-24 12:53:34 +0530

    Added lab5

--- Lab4 - Copy/utils/stt.py
+++
@@ -1,7 +0,0 @@
-import whisper
-
-def transcribe_audio(audio_path):
-    """Transcribe audio file using Whisper"""
-    model = whisper.load_model("base")
-    result = model.transcribe(audio_path)
-    return result['text']
